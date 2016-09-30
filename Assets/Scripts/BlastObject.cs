e112d7a108392233ae0be3fdf564d56df9021b06
Campbell Davis <campbell.davis@thirty4.com>
2016-09-30 22:47:54 +0000

    make blast shots vanish if they go offscreen, add blank spray collisions script

--- Assets/Scripts/BlastObject.cs
+++ Assets/Scripts/BlastObject.cs
@@ -2,21 +2,36 @@
 using System.Collections;
 
 public class BlastObject : SpaceObject {
+  public float offScreenCheckFrequency = 1;
 
 	// Use this for initialization
 	void Start () {
-	
+    InvokeRepeating("CheckOffScreen", offScreenCheckFrequency, offScreenCheckFrequency);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
   void OnTriggerEnter2D(Collider2D other)
   {
     if (other.gameObject.CompareTag("Enemy") || other.gameObject.CompareTag("HealthPickup")
        || other.gameObject.CompareTag("MovementPickup")) {
+      Destroy(gameObject);
+    }
+  }
+
+  void CheckOffScreen()
+  {
+    if (!GetComponent<Renderer>().isVisible) {
+      StartCoroutine(DestroyMe());
+    }
+  }
+
+  IEnumerator DestroyMe() {
+    yield return new WaitForSeconds(2f);
+    if (!GetComponent<Renderer>().isVisible) {
       Destroy(gameObject);
     }
   }
